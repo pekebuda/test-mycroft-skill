d1a40b961f4c1d55c0c6c2ed3620e408560888b9
Fernando Diaz <f.diazlaclaustra@umlaud.es>
2020-03-22 01:56:07 +0100

    Implementa intents y dialogs de bab5061 y 926c9be en la clase TestSkill.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,43 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from adapt.intent import IntentBuilder
 
 
-class Test(MycroftSkill):
+class TestSkill(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        """
+        called when the Skill is first constructed. It is often used to declare
+        variables or perform setup actions, however it cannot utilize other
+        MycroftSkill methods as the class does not yet exist
+        """
+        super().__init__(self)
 
-    @intent_file_handler('test.intent')
-    def handle_test(self, message):
-        self.speak_dialog('test')
+    def initialize():
+        """
+        Perform any final setup needed for the skill here. This function is
+        invoked after the skill is fully constructed and registered with the
+        system. Intents will be registered and Skill settings will be
+        available.
+        """
+        pass
+
+    def stop():
+        """
+        What your Skill should do if a stop intent is detected
+        """
+        pass
+
+    @intent_file_handler('TellMeYourAge.intent')
+    def handle_tell_me_your_age_intent(self, message):
+        self.speak_dialog('tell.me.your.age')
+
+    @intent_handler(IntentBuilder('PluckANumberIntent').require('PluckANumber.voc'))
+    def handle_thank_you_intent(self, message):
+        number_domain = message.data.get('domain')
+        if number_domain is not None:
+            self.speak_dialog('pluck.a.number')
+        else:
+            self.speak_dialog('pluck.a.number')
 
 
 def create_skill():
-    return Test()
-
+    return TestSkill()
